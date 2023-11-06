ef22ff941f9d4e61ad53ef56b398bb9b9522ca88
huynhthanhphat <thanhphat02122003@gmail.com>
2023-11-07 03:45:16 +0700

    push_php_forum

--- php_sql/menu.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    $result = chayTruyVanTraVeDL ($link, "select * from tbl_danhmuc"); 
-    while($rows=mysqli_fetch_assoc($result)){ 
-        echo " <li><a href='?dm=".$rows['id']."'>".$rows['ten']."</a></li>"; 
-    }
-?>
--- php_sql_forum/config.php
+++ php_sql_forum/config.php
@@ -1,6 +1,6 @@
 <?php
     define("HOST","localhost");
-    define("DB","db_hanghoa");
+    define("DB","db_forum");
     define("USER","root");
     define("PASSWORD","");
 ?>
